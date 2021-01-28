db1bc36385d0d2f6b010f64d5110ebaf73c91521
IPlayZed <pauljouser@gmail.com>
2021-01-28 17:47:24 +0100

    Reformatted User DAO and added interface method for insertion.
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- src/main/java/com/iplayzed/webapp/daos/UserDAO.java
+++ src/main/java/com/iplayzed/webapp/daos/UserDAO.java
@@ -8,12 +8,6 @@ import java.util.UUID;
  */
 public interface UserDAO {
     /***
-     * Updates the user's id.
-     * @param name The name to be changed to.
-     * @param user User to be updated.
-     */
-    void updateUser(String name, User user);
-    /***
      * Creates a new user with a random ID.
      * @param name the new User's name.
      * @return the new User object.
@@ -21,5 +15,15 @@ public interface UserDAO {
     default User createUser(String name) {
         return new User(UUID.randomUUID(), name);
     }
-
+    /***
+     * Inserts a user into the database.
+     * @param user User object to be inserted.
+     */
+    void insertUser(User user);
+    /***
+     * Updates the user's id.
+     * @param name The name to be changed to.
+     * @param user User to be updated.
+     */
+    void updateUser(String name, User user);
 }
