f9f79575bb602b9152dab31748539c6e9f2df1b2
IPlayZed <pauljouser@gmail.com>
2021-01-28 20:36:07 +0100

    Implemented REST API request handling
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- src/main/java/com/iplayzed/webapp/api/UserController.java
+++ src/main/java/com/iplayzed/webapp/api/UserController.java
@@ -1,12 +1,17 @@
 package com.iplayzed.webapp.api;
 
+import com.iplayzed.webapp.models.User;
 import com.iplayzed.webapp.services.DummyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /***
- *
+ * Implements HTTP requests using REST API.
  */
+@RequestMapping("api/v1/user")
 @RestController
 public class UserController {
     private final DummyUserService dummyUserService;
@@ -16,7 +21,12 @@ public class UserController {
         this.dummyUserService = dummyUserService;
     }
 
-    public void addUser(String name) {
-        dummyUserService.addUser(name);
+    /***
+     *
+     * @param user adds
+     */
+    @PostMapping
+    public void addUser(@RequestBody User user) {
+        dummyUserService.addUser(user);
     }
 }
